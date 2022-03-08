5716ca5d0fa29b75d10e3d1e826e298331a8233d
Muhammed Aly <40716171+muhammed3ly@users.noreply.github.com>
2022-03-08 15:42:31 +0200

    updated index.html and index.css and style.css

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'blogs/index.html')
+
+
+def posts(request):
+    return HttpResponse('posts')
+
+
+def post_detail(request, slug):
+    return HttpResponse('post by id: ' + slug)
