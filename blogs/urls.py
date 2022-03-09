298301bc9f2fdfc1a2fdd99e4f4e58ea7e3bef77
Muhammed Aly <40716171+muhammed3ly@users.noreply.github.com>
2022-03-09 10:57:24 +0200

    finished ui logic

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts', views.posts, name='posts'),
+    path('posts', views.getPosts, name='posts'),
     path('posts/<slug:slug>', views.post_detail, name='post-detail')
 ]
